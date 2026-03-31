5f871ee0926a605de44eb44039ca35feae9e3b73
ABU-RAYHAN-KOBIR <aburayhankobir013@gmail.com>
2026-03-31 14:12:05 +0600

    Fixed some problems

--- src/components/CardContainer.jsx
+++ src/components/CardContainer.jsx
@@ -12,16 +12,16 @@ export default function CardContainer() {
     }
   }
   return (
-    <div className="container mx-auto border border-red-500 min-h-screen flex flex-col gap-4 mt-12.5">
-      <div className="border border-red-500 flex-none text-center space-y-4">
+    <div className="container mx-auto min-h-screen flex flex-col gap-4 mt-12.5">
+      <div className="flex-none text-center space-y-4">
         <h1 className="text-3xl font-bold">Premium Digital Tools</h1>
         <p className="font-semibold">
           Choose from our curated collection of premium digital products
           designed to boost your productivity and creativity.
         </p>
-        <div className="border border-red-500 space-x-4">
-          <button className={`w-25 py-2 rounded-full font-bold cursor-pointer border-2 ${buttonIsActive.isProductsBtnActive?`bg-violet-800 text-white`: `bg-white text-black`}`} onClick={()=>toggling("productBtn")}>Products</button>
-          <button className={`w-25 py-2 rounded-full font-bold cursor-pointer border-2 ${buttonIsActive.isCartBtnActive?`bg-violet-800 text-white`: `bg-white text-black`}`} onClick={()=>toggling("cartBtn")}>Cart(2)</button>
+        <div className="space-x-4">
+          <button className={`w-25 py-2 rounded-full font-bold cursor-pointer border-2 shadow ${buttonIsActive.isProductsBtnActive?`bg-violet-800 text-white`: `bg-white text-black`}`} onClick={()=>toggling("productBtn")}>Products</button>
+          <button className={`w-25 py-2 rounded-full font-bold cursor-pointer border-2 shadow ${buttonIsActive.isCartBtnActive?`bg-violet-800 text-white`: `bg-white text-black`}`} onClick={()=>toggling("cartBtn")}>Cart(2)</button>
         </div>
       </div>
       <div className="border border-red-500 flex-1 max-w-5xl mx-auto w-full">I am card container.</div>
